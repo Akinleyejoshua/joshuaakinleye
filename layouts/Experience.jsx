c7a2c4ce891bab9969eccef72305c64e9c59180c
Joshua <akinleyejoshua.dev@gmail.com>
2024-02-21 04:20:32 +0100

    Add files via upload

--- layouts/Experience.jsx
+++ layouts/Experience.jsx
@@ -1,31 +1,45 @@
 import { FaBriefcase } from "react-icons/fa";
 
 export const Experience = () => {
+    const experiences = [
+        {
+            name: "C.A.C",
+            role: "Web Developer",
+            time: "2020 - 2021"
+        },
+        {
+            name: "Gtext",
+            role: "Full Stack Developer",
+            time: "2021 - 2022"
+        },
+        {
+            name: "Smart Technique",
+            role: "Conversational A.I Scientist (Data Science)",
+            time: "2022 - 2023"
+        },
+        {
+            name: "C.A.C",
+            role: "Senior Backend Developer",
+            time: "2024 • Current"
+        }
+    ]
     return <section className="experience" id="experience">
         <h1 className="row items-center">
             <FaBriefcase className="icon" />
             <div className="space"></div>
             <div className="space"></div>
             <div className="space"></div>
-           My Experiences
+            My Experiences
         </h1>
 
         <div className="experiences grid">
-            <div className="item">
-                <h2>Gtext</h2>
-                <p className="dim">Junior Full Stack Developer</p>
-                <h3>2021-2022</h3>
-            </div>
-            <div className="item">
-                <h2>Smart Technique</h2>
-                <p className="dim">Coversationsal A.I Developer</p>
-                <h3>2022-2023</h3>
-            </div>
-            <div className="item">
-                <h2>BLNR (Open Source)</h2>
-                <p className="dim">Senior Backend Developer</p>
-                <h3>2024 • Current</h3>
-            </div>
+            {experiences.map((item, i) => {
+                return <div className="item">
+                    <h2>{item.name}</h2>
+                    <p className="dim">{item.role}</p>
+                    <h3>{item.time}</h3>
+                </div>
+            })}
 
         </div>
 
