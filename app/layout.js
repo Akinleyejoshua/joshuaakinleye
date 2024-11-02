1186546bf810823a5a1c755e08af97ed12b8d909
Joshua <akinleyejoshua.dev@gmail.com>
2024-11-03 00:46:37 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,9 @@
 import "./globals.css";
-// import "@fontsource/poppins/100.css";
 
 export const metadata = {
   title: "Akinleye Joshua",
   description:
-    "Akinleye Joshua, Software Developer, I will help craft you a full stack web application and also build and deploy machine learning algorithms for your business",
+    "Akinleye Joshua, Software Developer, I will help craft you a full stack web application and also build and deploy machine learning algorithms for your business. As a Lead full-stack developer and ML Engineer, I possess a diverse skill set, proficient in both front-end and back-end and data-science technologies.",
 };
 
 export default function RootLayout({ children }) {
