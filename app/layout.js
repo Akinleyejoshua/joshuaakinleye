0d2f944e9d6c1df4f909a0f2bb158dbd6803611a
Joshua <akinleyejoshua.dev@gmail.com>
2024-11-03 00:59:20 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -4,8 +4,8 @@ export const metadata = {
   title: "Akinleye Joshua",
   description:
     "The Creative Software Developer, I will help craft you a full stack web application and also build and deploy machine learning algorithms for your business. As a Lead full-stack developer and ML Engineer, I possess a diverse skill set, proficient in both front-end and back-end and data-science technologies.",
-  icons: {
-    icon: ["https://akinleyejoshua.vercel.app/icon.jpg"]
+  openGraph: {
+    images: ["https://akinleyejoshua.vercel.app/icon.jpg"]
   }
 };
 
